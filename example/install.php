d1f0ce3f27ff1b7290a28e5507e38aa3624feb0d
Juergen Schwind <info@jbs-newmedia.de>
2022-05-30 21:49:01 +0200

    Example

---
+++ example/install.php
@@ -0,0 +1,22 @@
+<?php
+
+$path=realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR;
+$lib_path=realpath(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'src').DIRECTORY_SEPARATOR;
+
+require_once $lib_path.'Installer.php';
+
+use \JBSNewMedia\GitInstall\Installer;
+
+$Installer=new Installer();
+$Installer->setRealPath($path);
+$Installer->setLocalVersionFile('gitinstall.json');
+$Installer->setLocalRunningFile('gitinstall.run');
+$Installer->setName('jbsnewmedia/gitinstall');
+$Installer->setGit('github');
+$Installer->setUrl('https://api.github.com/repos/jbs-newmedia/GITInstall/releases');
+$Installer->setRelease('stable');
+$Installer->setRemotePath('src');
+$Installer->setLocalPath('../src');
+$Installer->setAction('execute');
+
+?>
